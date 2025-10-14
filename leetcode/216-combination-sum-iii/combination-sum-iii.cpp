6f14e5802221ff55e563db618530ba997cc07997
Jay shah <166749819+Jay-1409@users.noreply.github.com>
2025-10-14 09:39:01 +0530

    Time: 3 ms (14.59%) | Memory: 11.8 MB (10.03%) - LeetSync

---
+++ leetcode/216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        set<vector<int>> st;
+        set<int> used;
+        auto f = [&](set<int> &used, vector<int> &v,int sum, int loc, int num, auto && self) -> void {
+            if(num > 9)
+                return;
+            if(sum > n) {
+                return;
+            }
+            if(loc >= k) {
+                 if(sum == n)
+                    st.insert(v);
+                return;
+            }
+            // take
+            if(used.find(num) == used.end()) {
+                used.insert(num);
+                v.push_back(num);
+                self(used, v, sum + num, loc + 1, num, self);
+                used.erase(num);
+                v.pop_back();
+            }
+            // not take 
+            self(used, v, sum, loc, num + 1, self);
+        };
+        vector<int> v;
+        f(used, v, 0, 0, 1, f);
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
