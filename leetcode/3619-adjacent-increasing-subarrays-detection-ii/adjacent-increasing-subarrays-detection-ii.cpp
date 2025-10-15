3aad64edbb2f0c43f9c395f64cd2d586847893af
Jay shah <166749819+Jay-1409@users.noreply.github.com>
2025-10-15 06:23:53 +0530

    Time: 212 ms (87.97%) | Memory: 173.6 MB (89.47%) - LeetSync

--- leetcode/3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
+++ leetcode/3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
     int maxIncreasingSubarrays(vector<int>& nums) {
-        // binary search on answer and then to validate just one O(n) pass
-        int lo = 1;
-        int hi = (int)nums.size();
-        int ans = 1;
-        while(lo <= hi) {
-            int k = (hi + lo) / 2;
-            int cnt = 1;
-            int valid = 0;
-            bool poss = false;
-            auto f = [&]() -> void {
-                if(cnt >= 2 * k) {
-                    // can have both made
-                    poss |= true;
-                } else if(cnt >= k) {
-                    // one valid 
-                    cnt = 1;
-                    valid++;
-                    if(valid == 2) {
-                        // two valid found and they are contigious 
-                        poss |= true;
-                    }
-                } else {
-                    // seq broken 
-                    cnt = 1;
-                    valid = 0;
-                }
-            };
-            for(int i = 1; i < (int)nums.size(); ++i) {
-                if(nums[i] > nums[i - 1]) 
-                    cnt++;
-                else {
-                    f();
-                }
-            }
-            f();
-            if(poss) {
-                lo = k + 1;
-                ans = max(ans, k);
+        int cnt = 1;
+        int prevcnt = 0;
+        int ans = 0;
+        for(int i = 0; i < (int)nums.size() - 1; ++i) {
+            if(nums[i + 1] > nums[i]) {
+                cnt++;
+                ans = max(ans, cnt / 2);
             } else {
-                hi = k - 1;
+                prevcnt = cnt;
+                cnt = 1 ;
+            }
+            if(prevcnt >= cnt) {
+                // can form a inc seq one form the prev sequence since that length is greater than cnt 
+                ans = max(ans, cnt);
             }
         }
         return ans;
