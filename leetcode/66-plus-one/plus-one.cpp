49a00be6cb8dbb4df73c1876d142e48005b494a3
Jay shah <166749819+Jay-1409@users.noreply.github.com>
2025-10-29 09:46:46 +0530

    Time: 2 ms (7.23%) | Memory: 11.5 MB (78.53%) - LeetSync

---
+++ leetcode/66-plus-one/plus-one.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int nextcarry = 1;
+        for(int i = digits.size() - 1; i >= 0; i--){
+            digits[i] += nextcarry;
+            nextcarry = max(digits[i] - 9, 0);
+            if(digits[i] >= 10 && i > 0){
+                digits[i] -= 10;
+            }
+            cout << digits[i] << endl;
+        }           
+        reverse(digits.begin(),digits.end());
+        if(digits.back() >= 10){
+            int val = digits.back() % 10;//0
+            digits.back() /= 10;
+            int val2 = digits.back() % 10;//1
+            digits.back() = val;
+            digits.push_back(val2);
+            // reverse(digits.begin(),digits.end());
+        }
+            reverse(digits.begin(),digits.end());
+        return digits;
+    }
+};
