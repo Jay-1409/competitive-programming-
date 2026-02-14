56da3da9f811bf654007e122ebf17d0822ec02f1
Jay shah <166749819+Jay-1409@users.noreply.github.com>
2026-02-14 09:56:13 +0530

    Time: 25 ms (5.08%) | Memory: 45.4 MB (25.18%) - LeetSync

---
+++ leetcode/815-champagne-tower/champagne-tower.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> arr(100, vector<double>(100, 0));
+        arr[0][0] = poured;
+        arr[1][0] = arr[1][1] = max(0.0, (poured - 1) / 2.0);
+        arr[0][0] = min(arr[0][0], 1.0);
+        for(int i = 2; i < 100; ++i) {
+            arr[i][0] = max(0.0, (arr[i - 1][0] - 1) / 2.0);
+            arr[i][i] = max(0.0, (arr[i - 1][i - 1] - 1) / 2.0);
+            for(int j = 1; j < i; ++j) {
+                arr[i][j] = (max(0.0,(arr[i - 1][j] - 1) / 2.0) + max(0.0,(arr[i - 1][j - 1] - 1) / 2.0));
+            }
+            for(int j = 0; j <= i; ++j) {
+                arr[i - 1][j] = min(arr[i - 1][j], 1.0);
+            }
+        }
+        // for(int i = 0; i <= query_row; ++i) {
+        //     for(int j = 0; j <= query_row; ++j) {
+        //         cout << arr[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return arr[query_row][query_glass];
+    }
+};
