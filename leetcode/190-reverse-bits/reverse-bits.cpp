d66fe824052402fa53dce24785b27a4b4788b971
Jay shah <166749819+Jay-1409@users.noreply.github.com>
2026-02-16 10:53:54 +0530

    Time: 4 ms (29.28%) | Memory: 8.3 MB (15.72%) - LeetSync

--- leetcode/190-reverse-bits/reverse-bits.cpp
+++ leetcode/190-reverse-bits/reverse-bits.cpp
@@ -3,9 +3,8 @@ public:
     int reverseBits(int n) {
         int rev = 0;
         for(int i = 0; i < 32; ++i) {
-            int new_loc = (32 - i - 1);
             if((n >> i) & 1) {
-                rev |= (1 << new_loc);
+                rev |= (1 << (32 - i - 1));
             }
         }
         return rev;
