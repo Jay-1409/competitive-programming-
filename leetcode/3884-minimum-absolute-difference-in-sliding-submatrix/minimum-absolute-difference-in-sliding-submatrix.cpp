3a5127e4b842165cbc8a440769f6ca25550339ea
Jay shah <166749819+Jay-1409@users.noreply.github.com>
2026-03-20 06:48:33 +0530

    Time: 7 ms (96.30%) | Memory: 32 MB (81.48%) - LeetSync

--- leetcode/3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
+++ leetcode/3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -6,20 +6,22 @@ public:
         vector<vector<int>> ans(m - k + 1, vector<int>(n - k + 1));
         for(int i = 0; i < m - k + 1; ++i) {
             for(int j = 0; j < n - k + 1; ++j) {
-                set<int> temp;
+                vector<int> temp;
                 for(int a = i; a < i + k; ++a) {
                     for(int b = j; b < j + k; ++b) {
-                        temp.insert(grid[a][b]);
+                        temp.push_back(grid[a][b]);
                     }
                 }
+                sort(temp.begin(), temp.end());
                 int best_diff = INT_MAX;
                 if((int)temp.size() == 1)
                     best_diff = 0;
-                vector<int> vtemp(temp.begin(), temp.end());
-                for(int k = 0; k < (int)vtemp.size() - 1; ++k) {
-                    best_diff = min(best_diff, abs(vtemp[k + 1] - vtemp[k]));
+                for(int k = 0; k < (int)temp.size() - 1; ++k) {
+                    if(temp[k] == temp[k + 1])
+                        continue;
+                    best_diff = min(best_diff, abs(temp[k + 1] - temp[k]));
                 }
-                ans[i][j] = best_diff;
+                ans[i][j] = (best_diff == INT_MAX ? 0 : best_diff);
             }
         }
         return ans;
