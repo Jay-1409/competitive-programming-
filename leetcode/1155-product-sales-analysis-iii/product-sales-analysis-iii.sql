4b7b0c583cfed44b0e1fef8470ae7deccc1eb59e
Jay shah <166749819+Jay-1409@users.noreply.github.com>
2026-04-04 19:44:09 +0530

    Time: 503 ms (53.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,2 @@
+/* Write your PL/SQL query statement below */
+select s.product_id, s.year as first_year, s.quantity, s.price from sales s where s.year = (select min(year) from sales where product_id = s.product_id);
